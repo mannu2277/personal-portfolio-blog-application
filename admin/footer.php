a8b1190b848fd7dd0bf9856d8480a68567843aa7
Mannu Kumar <mannukumar7891@gmail.com>
2019-09-14 18:18:45 +0530

    2nd update

--- admin/footer.php
+++
@@ -1,9 +0,0 @@
- <footer>
-  
-          <div class="pull-right">
-         CMS Developed by <a href="https://www.linkedin.com/in/
-mannu-kumar-7a007015a
-" > <b>Mannu Kumar</b></a>
-          </div>
-          <div class="clearfix"></div>
- </footer>
